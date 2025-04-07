d7e9500e4a17fa179decf4d22ada644350291c31
Haileamlak Waleligne <haileamlakwaleligne3910@gmail.com>
2025-04-07 08:58:11 +0300

    update the build

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  // Optional configurations
+  // trailingSlash: true,
+  // distDir: 'dist',
+};
+
+export default nextConfig;
