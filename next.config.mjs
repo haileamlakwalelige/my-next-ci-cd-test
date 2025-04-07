ad460daaf16c9a1a8076a9b8af55cb970a32a215
Haileamlak Waleligne <haileamlakwaleligne3910@gmail.com>
2025-04-07 08:44:03 +0300

    update the main yml section six

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+const nextConfig = {
+    output: 'export',
+}
 
-export default nextConfig;
+module.exports = nextConfig
+
